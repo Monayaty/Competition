3dd37c525835cd881dabd21cce87b0a0a1793c5f
Monayaty <72939693+Monayaty@users.noreply.github.com>
2021-09-11 20:56:51 +0200

    adding challenges of chapter 4 and 5

---
+++ course1/chapter4/challenge-guessing game.py
@@ -0,0 +1,9 @@
+guessing = input("What's my favorite food?")
+
+if (guessing == "Pizza"):
+	print('Yep! So amazing!')
+
+else:
+	print("Yuck! That's not it!")
+
+print('Thanks for playing!')
---
+++ course1/chapter5/challenge-Favorite-cities.py
@@ -0,0 +1,6 @@
+def favorite_city(name):
+	print('One of my favorite cities is ', name)
+
+favorite_city('Alexandria')
+favorite_city('Sydney')
+favorite_city('Canbra')
