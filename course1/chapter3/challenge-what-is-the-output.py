cdacaef848158b5801e9be2b513f18a555539a21
Monayaty <72939693+Monayaty@users.noreply.github.com>
2021-09-11 13:32:11 +0200

    first commit

---
+++ course1/chapter3/challenge-what-is-the-output.py
@@ -0,0 +1,19 @@
+# First print challenge 1
+
+print('Challenge 1:')
+
+# print a message for the user
+
+message = "This is going to be tricky ;)"
+Message = "Very tricky"
+
+print(message) # It means show my message
+
+# Perform mathematical operations 
+result = 2**3
+print("2**3 = ", result)
+result = 5-3
+
+#print("5-3 = ", result)
+
+print("Challenge Completed!")
