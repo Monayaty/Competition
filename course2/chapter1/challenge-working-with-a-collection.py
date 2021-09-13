0087e3d26392a3362f27bb6276d080450ca9a7bd
Monayaty <72939693+Monayaty@users.noreply.github.com>
2021-09-13 16:48:11 +0200

    course2 challenges

---
+++ course2/chapter1/challenge-working-with-a-collection.py
@@ -0,0 +1,24 @@
+### Stars
+stars = 
+[
+	'Sol',
+	'Alpha Centauri',
+	'Barnard',
+	'Wolf 359'
+]
+print(stars[0])
+
+
+
+### Peaks
+peaks = 
+{
+	'African': 'Kilimanjaro',
+	'Antarctic': 'Vinson',
+	'Australian': 'Puncak jaya',
+	'Eurasian': 'Everest',	
+	'North_American': 'Denali',
+	'Pacific': 'Mauna Kea',	
+	'South_American': 'Aconcagua'
+}
+print(peaks[Australian])
